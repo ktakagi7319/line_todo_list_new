0b1fdd104c6f914a14ebb2ba3edae1943c681a67
高木健吾 <takagiastro@gmail.com>
2022-07-17 00:21:14 +0900

    <Task5>LINEで送ったメッセージをタスクとして登録するAPIを作成する。 完了しました

---
+++ app/controllers/line_bot_controller.rb
@@ -0,0 +1,6 @@
+class LineBotController < ApplicationController
+    # GET /tasks or /tasks.json
+    def callback
+      binding.pry
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "tasks#index"
+  post 'callback', to: "line_bot#callback"
 end
